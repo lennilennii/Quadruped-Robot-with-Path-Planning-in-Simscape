3650dd5eb2e54a1a685a9fcf4b00462237006161
Andres Renjifo <andres.renjifo@tum.de>
2024-09-05 22:49:37 +0200

    Update gait libraries and commented unconnected blocks and non working function

--- Workflows/complete_robot/plot_c_over_t.m
+++ Workflows/complete_robot/plot_c_over_t.m
@@ -1,9 +1,9 @@
-time = out.control_variable.time;
-data = out.control_variable.signals.values;
-
-plot(time, data);
-xlabel('Time');
-ylabel('Control value');
-title('Control value over time');
+% time = out.control_variable.time;
+% data = out.control_variable.signals.values;
+% 
+% plot(time, data);
+% xlabel('Time');
+% ylabel('Control value');
+% title('Control value over time');
 
 %saveas(gcf, 'control_over_t_plot.fig');

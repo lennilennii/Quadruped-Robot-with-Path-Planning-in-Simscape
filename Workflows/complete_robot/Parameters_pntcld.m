6b0e6f0869ec2c6a73f463ad685d1250dc21bd72
Andrew <ajammy1134@gmail.com>
2024-11-01 19:34:27 +0100

    Add point clouds to Rear left leg

--- Workflows/complete_robot/Parameters_pntcld.m
+++ Workflows/complete_robot/Parameters_pntcld.m
@@ -11,3 +11,16 @@ pointDensity = 15;
 [X,Y,Z] = sphere(round(pointDensity));
 Ball.sphere_ptcld = unique([reshape(X*Ball.Radius,[],1) reshape(Y*Ball.Radius,[],1) reshape(Z*Ball.Radius,[],1)],'rows');
 
+%% Leg %%
+legLength = t_leg;  
+legWidth = t_leg;   
+legHeight = L;   
+Leg.PointCloud.MarkerRadius  = 5e-4*1.5;
+
+% Create X, Y, Z Grid Coordinates 
+[x, y, z] = ndgrid(linspace(-legLength/2, legLength/2, 10), ...
+                   linspace(-legWidth/2, legWidth/2, 5), ...
+                   linspace(0, legHeight, 15));
+
+% 将网格展开成点云矩阵
+leg_PointCloud = unique([x(:), y(:), z(:)], 'rows');

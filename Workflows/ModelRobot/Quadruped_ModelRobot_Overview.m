e63439697f57535a49734e40658b940c8224ee1a
Andres Renjifo <andres.renjifo@tum.de>
2025-01-29 15:23:18 +0100

    Change html file

--- Workflows/ModelRobot/Quadruped_ModelRobot_Overview.m
+++ Workflows/ModelRobot/Quadruped_ModelRobot_Overview.m
@@ -1,13 +1,13 @@
 %% Quadruped Robot Modeling with Simscape(TM)
 %
 % This repository contains a Simscape model of a quadrupeded robot able to
-% walk in flat and inclined terrain, with a environment that presents
-% different obstacles, and the posibility of automatic or interactive
+% walk on flat and inclined terrain, with a environment that presents
+% different obstacles, and the posibility of open-loop or interactive
 % control system.
 %
 % *Important files*
-%
+%  
 % # Complete Robot Model (Run): <matlab:open_system('complete_robot') Model>
-% # Point cloud generation file: <matlab:open_system('Parameters_pntcld.m') Script>
+% # Point cloud generation file: <matlab:edit('Parameters_pntcld.m') Script>
 
 % Copyright 2023-2024 The MathWorks(TM), Inc.
